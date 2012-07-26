27961b06363022c8c7a3329950d5c116de1cdde9
Naoki Nishiguchi <nnishiguchister@gmail.com>
2012-07-26 10:49:23 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.6'
+
+gem 'mysql2'
+gem 'haml-rails'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'tapp'
+  gem 'rspec-rails'
+
+  gem 'capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cleaner::Application.config.secret_token = '2cfaa2d91987c67c3c5827ad6ad09454a59496bd61a39d08278bea0b0f098025989a9f826103f6195e25ff2c0376398d354b73485c466f1dae7f7765bfeab09e'
