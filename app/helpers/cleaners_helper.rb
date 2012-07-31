44117595566a9ca41f4e19105c3640d7e76e13b8
BABA Kazuki <kbaba1001@gmail.com>
2012-07-31 10:33:18 +0900

    文字数を少なくして、取りあえず動くようにはなった

--- app/helpers/cleaners_helper.rb
+++ app/helpers/cleaners_helper.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 module CleanersHelper
   def mailto()
-    "mailto:sp@esm.co.jp?subject='#{mail_subject}'&amp;body='#{mail_body}'"
+    "mailto:sp@esm.co.jp?subject=#{mail_subject}&amp;body=#{mail_body}"
   end
 
   private
@@ -12,17 +12,17 @@ module CleanersHelper
   private
   def mail_body
 <<-EOD
-【テーブル拭き】
-　→ #{@wipingers.map(&:name).join(', ')}
-　　*期間中3回を目安に（月・水・金など）できるだけ朝お願いします*
+* テーブル拭き
+#{@wipingers.map(&:name).join(', ')}
+期間中3回を目安に（月・水・金など）できるだけ朝お願いします
 
-【ゴミ捨て】
-　→ #{@thowers.map(&:name).join(', ')}
-　　*期間中2回を目安に（火・木など）*
+* ゴミ捨て
+#{@thowers.map(&:name).join(', ')}
+期間中2回を目安に（火・木など）
 
-【掃除機】
-　→ #{@vacuumers.map(&:name).join(', ')}
-　　*期間中1回(9時前または18時30分以降)*
+* 掃除機
+#{@vacuumers.map(&:name).join(', ')}
+期間中1回(9時前または18時30分以降)
 EOD
   end
 end
