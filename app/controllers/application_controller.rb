faec8962e22ac194ee8228a6cd6b42d68f90c296
Naoki Nishiguchi <nnishiguchister@gmail.com>
2013-06-19 23:36:00 +0900

    開発時は見たいよね

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with :name => ENV["BASIC_AUTH_NAME"], :password => ENV["BASIC_AUTH_PASSWORD"]
+  http_basic_authenticate_with :name => ENV["BASIC_AUTH_NAME"], :password => ENV["BASIC_AUTH_PASSWORD"] if Rails.env.production?
 end
